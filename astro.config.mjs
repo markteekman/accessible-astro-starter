a810ac99b5fed4a74227bcb31cff28bef6ec8bd5
Mark Teekman <mark.teekman@gmail.com>
2022-10-06 14:21:07 +0200

    Cleanup project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({
-  vite: {
-    ssr: {
-      external: ['svgo']
-    }
-  }
-})
+export default defineConfig({})
