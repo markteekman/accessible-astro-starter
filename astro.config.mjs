0b5c93f178d346f8230196e5a21ad38874833a5f
Mark Teekman <markteekman@users.noreply.github.com>
2023-01-31 17:24:25 +0100

    Add eslint configs and packages and astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config'
+import compress from 'astro-compress'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()],
+  integrations: [compress(), mdx(), tailwind()],
 })
