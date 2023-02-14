90086d838bd276ac12d31004c77727d8b9d5b0b9
Mark Teekman <markteekman@users.noreply.github.com>
2023-02-14 19:42:11 +0100

    Add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config'
 import compress from 'astro-compress'
 import mdx from '@astrojs/mdx'
+import partytown from '@astrojs/partytown'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), mdx(), tailwind()],
+  integrations: [compress(), mdx(), partytown(), tailwind()],
 })
