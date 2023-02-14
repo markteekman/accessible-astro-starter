ba16b46f3010bf2ef6711a9ad08c25be8f86b8fa
Mark Teekman <markteekman@users.noreply.github.com>
2023-02-14 19:45:33 +0100

    Remove partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config'
 import compress from 'astro-compress'
 import mdx from '@astrojs/mdx'
-import partytown from '@astrojs/partytown'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), mdx(), partytown(), tailwind()],
+  integrations: [compress(), mdx(), tailwind()],
 })
