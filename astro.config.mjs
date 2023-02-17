d9fb8838949936a2f9f2eec013a0b5d2083861bb
Mark Teekman <markteekman@users.noreply.github.com>
2023-02-17 15:55:33 +0100

    Update dependencies and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config'
 import compress from 'astro-compress'
+import image from '@astrojs/image'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), mdx(), tailwind()],
+  integrations: [compress(), image(), mdx(), tailwind()],
 })
