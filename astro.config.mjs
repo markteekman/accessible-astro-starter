e2293d1e5ac7b5d90209d8782f142477d73c8907
= <otterlord.dev@gmail.com>
2023-08-13 18:37:10 +0100

    Upgrade astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
+import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind(), compress()],
 })
