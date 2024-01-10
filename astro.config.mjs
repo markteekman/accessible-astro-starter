b9ee376064af21fc2ef3d1bad2ee358234f72c41
Lyle Reed <lyleareed@gmail.com>
2024-01-09 19:18:54 -0600

    Add icon integration to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
+import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [mdx(), tailwind({
+  integrations: [mdx(), icon(), tailwind({
     applyBaseStyles: false,
   }), compress()],
 })
