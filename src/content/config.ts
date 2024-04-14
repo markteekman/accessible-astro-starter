07e0e06aa97947f6350054984f810566e89779d3
Samuel C. Tyler <skamansam@gmail.com>
2024-04-14 18:17:17 +0000

    try to setup an API

---
+++ src/content/config.ts
@@ -0,0 +1,22 @@
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `type` and `schema` for each collection
+const postsCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      description: z.string(),
+      author: z.string(),
+      order: z.number(),
+      image: z.object({
+        url: z.string(),
+        alt: z.string()
+      }),
+      tags: z.array(z.string())
+    })
+});
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+  posts: postsCollection,
+};
---
+++ src/content/posts/the-old-man.md
@@ -0,0 +1,19 @@
+---
+title: "Welcome to my blog"
+pubDate: 2024-01-01
+description: Welcommen
+author: samuel-c-tyler # references `src/content/authors/ben-holmes.json`
+order: 1
+image: 
+    url: "null"
+    alt: the old man
+tags: 
+    - old man
+    - characters
+    - deities
+relatedPosts:
+- about-me # references `src/content/blog/about-me.md`
+- my-year-in-review # references `src/content/blog/my-year-in-review.md`
+---
+
+# The Old Man
